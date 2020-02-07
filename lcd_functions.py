221123f093c75c8a2c9b9f83a2bfdff4728d8ba4
Alexandru Calmuc <calmucalexandru@gmail.com>
2020-02-07 12:40:08 +0000

    implemented Lcd Class with custom functions and added lcd display functionality to time_update.py

---
+++ lcd_functions.py
@@ -0,0 +1,41 @@
+import sys
+import time
+sys.path.insert(1, 'lcd')
+import lcddriver
+
+
+class Lcd():
+
+    def __init__(self):
+        self.display_ = lcddriver.lcd()
+
+
+    def display(self, text, line):
+        """
+        Parameters:(text to print, line number on which to print)
+        Return: function to display text on lcd display
+        """
+        self.display_.lcd_display_string(text, line)
+
+
+    def display_scrolling(self, text, line, num_scrolls=3, num_cols=16):
+        """
+        Parameters:(text to print, line number on which to print,
+                    number of times to scroll, number of columns of lcd display)
+        Return: function to display scrolling text on lcd display
+        """
+        for _ in range(num_scrolls):
+            if(len(text) > num_cols):
+                self.display_.lcd_display_string(text[:num_cols], line)
+                time.sleep(1)
+                for i in range(len(text) - num_cols + 1):
+                    str_to_print = text[i:i+num_cols]
+                    self.display_.lcd_display_string(str_to_print, line)
+                    time.sleep(0.2)
+                time.sleep(1)
+            else:
+                self.display_.lcd_display_string(text, line)
+
+
+    def clear(self):
+        self.display_.lcd_clear()
--- time_update.py
+++ time_update.py
@@ -1,17 +1,21 @@
+import sys
 import time
 import serial
 import logging
 import subprocess
+from datetime import datetime
 from serial.tools import list_ports
 
+# From current directory (lcd.py)
+from lcd_functions import Lcd
+
 BASE_DIR = '/home/pi/trackman/GPS-Tracker/'
-WAIT_TIME = 5  # seconds
+WAIT_TIME = 3  # seconds
 
 logging.basicConfig(filename=BASE_DIR+'logs/system_logs/time_update.log',
                     format='%(levelname)s: %(message)s',
                     level=logging.DEBUG)
 logger = logging.getLogger()
-
 
 def get_gps_port(manufacturer):
     """Gets the serial port on which the GPS sensor is transmitting data."""
@@ -24,6 +28,12 @@ def get_gps_port(manufacturer):
 
 
 if __name__ == '__main__':
+
+    lcd = Lcd()
+
+    lcd.display('Updating Time', 1)
+    lcd.display('Starting...', 2)
+    time.sleep(2)
     while True:
         logger.info('Starting GPS Time Update.')
         port = get_gps_port('u-blox')
@@ -33,14 +43,17 @@ if __name__ == '__main__':
                 ser.reset_input_buffer()
                 while True:
                     raw_data = ser.readline()
+
                     if raw_data[3:6] == b'RMC':
                         data = str(raw_data).split(',')
 
                         # check to see if gps data is present (signal strong enough)
                         if data[1] and data[9]:
-                            date = data[9][4:6] + '-' + data[9][2:4] + '-' + data[9][:2]
-                            time = data[1][:2] + ':' + data[1][2:4] + ':' + data[1][4:6]
-                            timestamp = date + ' ' + time
+                            lcd.display('GPS signal found', 2)
+                            time.sleep(2)
+                            _date = data[9][4:6] + '-' + data[9][2:4] + '-' + data[9][:2]
+                            _time = data[1][:2] + ':' + data[1][2:4] + ':' + data[1][4:6]
+                            timestamp = _date + ' ' + _time
 
                             try:
                                 subprocess.call(['sudo date -s "{}"'.format(timestamp)], shell=True)
@@ -50,12 +63,20 @@ if __name__ == '__main__':
                                 logger.info('System time has been set to: {}'.format(timestamp))
                                 break
                         else:
+                            lcd.display_scrolling('Waiting for GPS signal...', 2, num_scrolls=1)
                             logger.warning('GPS Signal weak. Waiting {} '
                                            'second(s) for stronger GPS signal...'.format(WAIT_TIME))
                             time.sleep(WAIT_TIME)  # Waiting for better GPS signal
+                            ser.reset_input_buffer()
         except IOError:
+            lcd.display_scrolling('Please connect the GPS Sensor', 2, num_scrolls=1)
             logger.warning('GPS Signal not found. Waiting {} second(s) for GPS signal...'.format(WAIT_TIME))
             time.sleep(WAIT_TIME)  # Waiting for GPS signal
         else:
             logger.info('GPS Time update finished successfully.')
+            lcd.display_scrolling('Done - GPS Time Update', 2, num_scrolls=1)
+            time.sleep(2)
+            lcd.display_scrolling('{} 20{} UTC'.format(_time[:5], _date), 2, num_scrolls=2)
+            time.sleep(3)
+            lcd.clear()
             break
