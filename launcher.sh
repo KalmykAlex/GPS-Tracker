d1fbb296e2a3cf428c0f524fd792c0ad7fa51a18
Alexandru Calmuc <calmucalexandru@gmail.com>
2020-02-09 07:34:03 +0000

    modified launcher to redirect stderr of api.py to api.log file

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Script that starts the tracker.py script at boot time
 
-sudo python3 /home/pi/trackman/GPS-Tracker/time_update.py 2> /home/pi/trackman/GPS-Tracker/logs/system_logs/startup_errors.log
+base_dir="/home/pi/trackman/GPS-Tracker"
+
+sudo python3 $base_dir/time_update.py 2> $base_dir/logs/system_logs/startup_errors.log
 sleep 3
-sudo python3 /home/pi/trackman/GPS-Tracker/tracker.py & 2> /home/pi/trackman/GPS-Tracker/logs/system_logs/startup_errors.log
-sudo python3 /home/pi/trackman/GPS-Tracker/api.py & 2> /home/pi/trackman/GPS-Tracker/logs/system_logs/startup_errors.log
+sudo python3 $base_dir/tracker.py & 2> $base_dir/logs/system_logs/startup_errors.log
+sudo python3 $base_dir/api.py & > $base_dir/logs/syste_logs/api.log 2> $base_dir/logs/system_logs/startup_errors.log
+
