ac73be18de5fab3dfb54b9987f97462f5c8e4464
Cyber1 <Cyber1@2574-CYBER-WS1.cyber.local>
2020-02-09 11:55:50 +0200

    fixed typo on launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -6,5 +6,5 @@ base_dir="/home/pi/trackman/GPS-Tracker"
 sudo python3 $base_dir/time_update.py 2> $base_dir/logs/system_logs/startup_errors.log
 sleep 3
 sudo python3 $base_dir/tracker.py & 2> $base_dir/logs/system_logs/startup_errors.log
-sudo python3 $base_dir/api.py & > $base_dir/logs/syste_logs/api.log 2> $base_dir/logs/system_logs/startup_errors.log
+sudo python3 $base_dir/api.py & > $base_dir/logs/system_logs/api.log 2> $base_dir/logs/system_logs/startup_errors.log
 
