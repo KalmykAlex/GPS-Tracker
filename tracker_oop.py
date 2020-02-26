0943061206181ae27bb38b569637243bc9466ffd
Alexandru Calmuc <calmucalexandru@gmail.com>
2020-02-26 13:09:03 +0000

    working version of tracker without logging, lcd and buzzer

--- tracker_oop.py
+++ tracker_oop.py
@@ -15,7 +15,7 @@ class RFID(threading.Thread):
     id_queue = queue.Queue()
     start_signal = threading.Semaphore(0)
     stop_signal = threading.Semaphore(0)
-    id_database = ['780870559455', '142189814135']
+    id_database = [780870559455, 142189814135]
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -27,6 +27,7 @@ class RFID(threading.Thread):
             id_list.append(self.reader.read_id())  # blocking
             if len(id_list) == 1:
                 print('Start Route')
+                self.id_queue.put(id_list[0])
                 self.start_signal.release()
             if not all([id in self.id_database for id in id_list]):
                 id_list.pop()
@@ -38,18 +39,17 @@ class RFID(threading.Thread):
                 id_list.clear()
                 print('Stop Route')
                 self.stop_signal.release()
-            print(id_list)
             time.sleep(3)
+
 
 class GPS(threading.Thread):
     """Producer Thread"""
-    data_queue = queue.Queue()
+    data_queue = queue.Queue(maxsize=2)
     ublox_vid = '1546'
 
     def __init__(self, vid=ublox_vid):
         threading.Thread.__init__(self)
-        if vid:
-            self.port = self.get_gps_port(vid)
+        self.port = self.get_gps_port(vid)
 
     def run(self):
         global shutdown
@@ -57,12 +57,12 @@ class GPS(threading.Thread):
             while not shutdown:
                 raw_data = ser.readline()
                 if raw_data[3:6] == b'RMC':
-                    self.extract_parameters(raw_data, self.data_queue)
-                    print(self.data_queue.qsize())
-                self.data_queue.join()
+                    self.data_queue.put(self.extract_parameters(raw_data))
+                    if self.data_queue.full():
+                        self.data_queue.get()
 
     @staticmethod
-    def extract_parameters(data, data_queue):
+    def extract_parameters(data):
         data = str(data).split(',')
         try:
             lat = round(float(data[3][:2]) + float(data[3][2:]) / 60, 6)
@@ -73,7 +73,7 @@ class GPS(threading.Thread):
         except ValueError:
             print('Waiting for GPS signal...')
         else:
-            data_queue.put([timestamp, lat, lon])
+            return (timestamp, lat, lon)
 
     @staticmethod
     def get_gps_port(vid):
@@ -86,59 +86,54 @@ class GPS(threading.Thread):
 
 class Journey:
     global shutdown
-    gps_buffer = []
+    gps_buffer = deque(maxlen=2)
+    request_data_event = threading.Event()
 
     def __init__(self, id_queue, data_queue, start_signal, stop_signal):
         self.start_signal = start_signal
         self.stop_signal = stop_signal
         self.data_queue = data_queue
         self.id_queue = id_queue
-        self.user_id = None
-        self.timestamp_start = None
-        self.timestamp_stop = None
-        self.lat_start = None
-        self.lon_start = None
-        self.lat_stop = None
-        self.lon_stop = None
         self.total_distance = 0
 
     def run(self):
         while not shutdown:
-
             self.start_signal.acquire()
+            self.user_id = self.id_queue.get()
             self.gps_buffer.append(self.data_queue.get())
-            if any([self.timestamp_start, self.lat_start, self.lon_start]) is None:
-                self.timestamp_start = self.gps_buffer[0][0]
-                self.lat_start = self.gps_buffer[0][1]
-                self.lon_start = self.gps_buffer[0][2]
-            self.total_distance = self.calculate_distance(self.gps_buffer,
-                                                          self.total_distance)
-            self.data_queue.task_done()
+            self.timestamp_start, self.lat_start, self.lon_start = self.gps_buffer[0]
 
-            self.stop_signal.acquire()
-            if any([self.timestamp_stop, self.lat_stop, self.lon_stop]) is None:
-                self.timestamp_stop = self.gps_buffer[1][0]
-                self.lat_stop = self.gps_buffer[1][1]
-                self.lon_stop = self.gps_buffer[1][1]
-            return {'user_id':         self.user_id,
-                    'timestamp_start': self.timestamp_start,
-                    'lat_start':       self.lat_start,
-                    'lon_start':       self.lon_start,
-                    'timestamp_stop':  self.timestamp_stop,
-                    'lat_stop':        self.lat_stop,
-                    'lon_stop':        self.lon_stop,
-                    }
+            while not self.stop_signal.acquire(blocking=False):
+                self.gps_buffer.append(self.data_queue.get())
+                self.total_distance = self.calculate_distance(self.gps_buffer, self.total_distance)
+                print('Distance: ', self.total_distance)
+
+            self.gps_buffer.append(self.data_queue.get())
+            self.timestamp_stop, self.lat_stop, self.lon_stop = self.gps_buffer[1]
+
+            print({'user_id':         self.user_id,
+                   'timestamp_start': self.timestamp_start,
+                   'lat_start':       self.lat_start,
+                   'lon_start':       self.lon_start,
+                   'timestamp_stop':  self.timestamp_stop,
+                   'lat_stop':        self.lat_stop,
+                   'lon_stop':        self.lon_stop,
+                   'distance':        self.total_distance
+                   })
+
+            self.gps_buffer.clear()
+            self.total_distance = 0
 
     @staticmethod
     def calculate_distance(gps_data, total_distance):
         if len(gps_data) == 2:
-            d_dist = geodesic(gps_data[0][1:], gps_data[1][1:])
+            d_dist = geodesic(gps_data[0][1:], gps_data[1][1:]).m
             if d_dist > 1:
                 total_distance = round(total_distance + d_dist, 2)
                 del gps_data[0]
             else:
                 del gps_data[1]
-        return total_distance
+            return total_distance
 
 
 shutdown = False
@@ -146,7 +141,10 @@ read = GPS()
 rfid = RFID()
 journey = Journey(rfid.id_queue, read.data_queue, rfid.start_signal, rfid.stop_signal)
 
+rfid.start()
 read.start()
-journey.run()
-shutdown = True
+while not shutdown:
+    info = journey.run()
+    print(info)
 read.join()
+rfid.join()
