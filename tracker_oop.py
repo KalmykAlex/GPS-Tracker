7a65970a6d522fc00854d72aa59d7c397b45f3c4
Calmuc Alexandru <calmucalexandru@gmail.com>
2020-02-28 21:14:50 +0200

    added mixins and better docstrings

--- tracker_oop.py
+++ tracker_oop.py
@@ -1,34 +1,60 @@
-import csv
 import os
-import serial
-import json
+import csv
 import glob
-import threading
+import json
+import time
 import queue
-from mfrc522 import SimpleMFRC522
-from geopy.distance import geodesic
-from serial.tools import list_ports
+import threading
 from collections import deque
 
-import time
+import serial
+from serial.tools import list_ports
+from geopy.distance import geodesic
+from mfrc522 import SimpleMFRC522
 
 
-class RFID(threading.Thread):
-    """Signaling Thread"""
-    reader = SimpleMFRC522()
-    id_queue = queue.Queue()
+# Declaring MixIn's for Thread Events and Queues ##############################
+
+class ShutdownMixin:
+    shutdown = threading.Event()
+
+
+class UnexpectedShutdownMixin:
+    unexpected_shutdown = threading.Event()
+
+
+class SemaphoreMixin:
     start_signal = threading.Semaphore(0)
     stop_signal = threading.Semaphore(0)
+
+
+class IDQueueMixin:
+    id_queue = queue.Queue()
     id_database = [780870559455, 142189814135]
 
-    def __init__(self, unexpected_shutdown_event):
+
+class DataQueueMixin:
+    data_queue = queue.Queue(maxsize=2)
+
+
+# Declaring Worker Threads ####################################################
+
+class RFID(threading.Thread,
+           ShutdownMixin,
+           UnexpectedShutdownMixin,
+           SemaphoreMixin,
+           IDQueueMixin):
+    """Thread that handles RFID card reads and
+    signals to the main thread accordingly.
+    """
+    reader = SimpleMFRC522()
+
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.unexpected_shutdown = unexpected_shutdown_event
 
     def run(self):
-        global shutdown
         id_list = deque(maxlen=2)
-        while not shutdown:
+        while not self.shutdown.is_set():
             id_list.append(self.reader.read_id())  # blocking
             if len(id_list) == 1 and not self.unexpected_shutdown.is_set():
                 print('Start Route')
@@ -48,9 +74,12 @@ class RFID(threading.Thread):
             time.sleep(3)
 
 
-class GPS(threading.Thread):
-    """Producer Thread"""
-    data_queue = queue.Queue(maxsize=2)
+class GPS(threading.Thread,
+          ShutdownMixin,
+          DataQueueMixin):
+    """Thread that gets GPS data and passes
+    it to the main thread through a queue.
+    """
     ublox_vid = '1546'
 
     def __init__(self, vid=ublox_vid):
@@ -58,10 +87,9 @@ class GPS(threading.Thread):
         self.port = self.get_gps_port(vid)
 
     def run(self):
-        global shutdown
         try:
             with serial.Serial(self.port) as ser:
-                while not shutdown:
+                while not self.shutdown.is_set():
                     raw_data = ser.readline()
                     if raw_data[3:6] == b'RMC':
                         self.data_queue.put(self.extract_parameters(raw_data))
@@ -72,7 +100,7 @@ class GPS(threading.Thread):
 
     @staticmethod
     def extract_parameters(data):
-        """Extracts the timestamp, latitude and longitude out of a RMC NMEA bytes object."""
+        """Extract the timestamp, latitude and longitude out of a RMC NMEA bytes object."""
         data = str(data).split(',')
         try:
             lat = round(float(data[3][:2]) + float(data[3][2:]) / 60, 6)
@@ -87,35 +115,36 @@ class GPS(threading.Thread):
 
     @staticmethod
     def get_gps_port(vid):
-        """Returns the serial port name of the GPS sensor with the specified vendor ID."""
+        """Return the serial port name of the GPS sensor with the specified vendor ID."""
         comports = [port for port in list_ports.comports()]
         for port in comports:
             if vid in port.hwid:
                 return port.device
 
 
-class Journey:
-    global shutdown
+class Journey(ShutdownMixin,
+              UnexpectedShutdownMixin,
+              SemaphoreMixin,
+              IDQueueMixin,
+              DataQueueMixin):
     gps_buffer = deque(maxlen=2)
     request_data_event = threading.Event()
     root_dir = '/home/pi/trackman/GPS-Tracker/'
-    gps_logs_folder = root_dir + 'logs/gps_logs/'
+    gps_logs_dir = root_dir + 'logs/gps_logs/'
 
-    def __init__(self, id_queue, data_queue, start_signal, stop_signal, unexpected_shutdown_event):
-        self.start_signal = start_signal
-        self.stop_signal = stop_signal
-        self.data_queue = data_queue
-        self.id_queue = id_queue
-        self.total_distance = 0
+    def __init__(self):
         self.user_id = None
+        self.route_id = None
+        self.total_distance = 0
+        self.timestamp, self.lat, self.lon = [None, None, None]
         self.timestamp_start, self.lat_start, self.lon_start = [None, None, None]
         self.timestamp_stop, self.lat_stop, self.lon_stop = [None, None, None]
-        self.timestamp, self.lat, self.lon = [None, None, None]
-        self.unexpected_shutdown = unexpected_shutdown_event
 
-    def __init_route_id(self):
+    def _init_route_id(self):
+        """Check for the last route ID from the routes
+        log file and initializes the new route ID."""
         try:
-            with open(self.gps_logs_folder + 'routes.log', 'r') as routes_log:
+            with open(self.gps_logs_dir + 'routes.log', 'r') as routes_log:
                 last_log = routes_log.readlines()[-1].strip()
                 last_route_id = json.loads(last_log)['route_id']
                 route_id = last_route_id + 1
@@ -123,10 +152,16 @@ class Journey:
             route_id = 1
         return route_id
 
-    def __log_to_csv(self):
+    def _log_as_csv(self):
+        """Create a CSV formatted log unique to each route
+        or append route data to an existing route log if an
+        unexpected system shutdown had occur.
+
+        CSV Log Format: timestamp,latitude,longitude,distance
+        """
         route_filename = f'routes/route_{self.route_id}_{self.user_id}.csv'
-        route_log_exists = os.path.isfile(self.gps_logs_folder + route_filename)
-        with open(self.gps_logs_folder + route_filename, 'a') as route_log:
+        route_log_exists = os.path.isfile(self.gps_logs_dir + route_filename)
+        with open(self.gps_logs_dir + route_filename, 'a') as route_log:
             headers = ['Timestamp', 'Latitude', 'Longitude', 'Total_Distance']
             writer = csv.DictWriter(route_log, delimiter=',',
                                     lineterminator='\n',
@@ -142,20 +177,24 @@ class Journey:
             route_log.flush()
             os.fsync(route_log)
 
-    def __check_unexpected_shutdown(self):
-        routes_folder = self.gps_logs_folder + 'routes/'
-        if self.route_id in [int(_.split('_')[1]) for _ in os.listdir(routes_folder)]:
+    def _check_unexpected_shutdown(self):
+        """If the current route ID appears in the name of a file inside
+        the routes folder that means an unexpected shutdown had occur.
+        """
+        routes_dir = self.gps_logs_dir + 'routes/'
+        if self.route_id in [int(_.split('_')[1]) for _ in os.listdir(routes_dir)]:
             return True
         else:
             return False
 
-    def __fix_unexpected_shutdown(self):
+    def _fix_unexpected_shutdown(self):
+        """Grab the route parameters from the last logged route."""
         self.unexpected_shutdown.set()
-        routes_folder = self.gps_logs_folder + 'routes/'
+        routes_dir = self.gps_logs_dir + 'routes/'
         self.user_id = os.path.basename(
-            glob.glob(routes_folder + f'route_{self.route_id}_*')[0]) \
+            glob.glob(routes_dir + f'route_{self.route_id}_*')[0]) \
             .split('_')[-1].split('.')[0]
-        with open(routes_folder + f'route_{self.route_id}_{self.user_id}.csv') as file:
+        with open(routes_dir + f'route_{self.route_id}_{self.user_id}.csv') as file:
             lines = file.read().splitlines()
             self.total_distance = float(lines[-1].split(',')[-1])
             self.timestamp_start = lines[1].split(',')[0]
@@ -167,7 +206,8 @@ class Journey:
                                     float(lines[-1].split(',')[2])
                                     ])
 
-    def __route_to_json(self):
+    def _route_to_json(self):
+        """Create a JSON route log and save it to routes.log."""
         route = {'route_id':        self.route_id,
                  'user_id':         self.user_id,
                  'timestamp_start': self.timestamp_start,
@@ -178,16 +218,16 @@ class Journey:
                  'lon_stop':        self.lon_stop,
                  'distance':        self.total_distance
                  }
-        with open(self.gps_logs_folder + 'routes.log', 'a') as routes_log:
+        with open(self.gps_logs_dir + 'routes.log', 'a') as routes_log:
             routes_log.write(json.dumps(route) + '\n')
             routes_log.flush()
             os.fsync(routes_log)
 
     def run(self):
-        while not shutdown:
-            self.route_id = self.__init_route_id()
-            if self.__check_unexpected_shutdown():
-                self.__fix_unexpected_shutdown()
+        while not self.shutdown.is_set():
+            self.route_id = self._init_route_id()
+            if self._check_unexpected_shutdown():
+                self._fix_unexpected_shutdown()
             else:
                 self.start_signal.acquire()
                 self.user_id = self.id_queue.get()
@@ -197,7 +237,7 @@ class Journey:
             while not self.stop_signal.acquire(blocking=False):
                 self.gps_buffer.append(self.data_queue.get())
                 self.timestamp, self.lat, self.lon = self.gps_buffer[1]
-                self.__log_to_csv()
+                self._log_as_csv()
                 gps_data = [data[1:] for data in self.gps_buffer]
                 self.total_distance = self.calculate_distance(gps_data, self.total_distance)
                 print('Distance: ', self.total_distance)
@@ -205,7 +245,7 @@ class Journey:
             self.gps_buffer.append(self.data_queue.get())
             self.timestamp_stop, self.lat_stop, self.lon_stop = self.gps_buffer[1]
 
-            self.__route_to_json()
+            self._route_to_json()
 
             # Cleanup
             self.gps_buffer.clear()
@@ -214,11 +254,12 @@ class Journey:
 
     @staticmethod
     def calculate_distance(gps_data, total_distance):
-        """Returns the distance traveled in the current journey.
+        """Return the distance traveled in the current journey.
 
         Arguments:
-            gps_data -- two GPS coordinates [[lat1, lon1], [lat2, lon2]] (in decimal degrees)
-            total_distance -- previously calculated travel distance (in meters)
+            gps_data        --  two GPS coordinates
+                                [[lat1, lon1], [lat2, lon2]] (in decimal degrees)
+            total_distance  --  previously calculated travel distance (in meters)
         """
         if len(gps_data) == 2:
             d_dist = geodesic(gps_data[0], gps_data[1]).m
@@ -233,19 +274,13 @@ class Journey:
 if __name__ == '__main__':
     root_dir = '/home/pi/trackman/GPS-Tracker/'
     unexpected_shutdown_event = threading.Event()
-    shutdown = False
     read = GPS()
-    rfid = RFID(unexpected_shutdown_event)
-    journey = Journey(rfid.id_queue,
-                      read.data_queue,
-                      rfid.start_signal,
-                      rfid.stop_signal,
-                      unexpected_shutdown_event
-                      )
+    rfid = RFID()
+    journey = Journey()
 
     rfid.start()
     read.start()
-    while not shutdown:
+    while not journey.shutdown.is_set():
         journey.run()
     read.join()
     rfid.join()
